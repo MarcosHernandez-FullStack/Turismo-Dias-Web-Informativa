280bc7ae6793b8a872276bd99efc4f97315e5e6a
Luis Guevara Aredo <guevaraluis03@hotmail.com>
2023-11-17 21:26:36 -0500

    2023.11.17a

--- resources/views/secciones/libro-reclamaciones.blade.php
+++ resources/views/secciones/libro-reclamaciones.blade.php
@@ -206,12 +206,7 @@
                                 <div
                                     class="elementor-element elementor-element-2a6264f9 elementor-widget elementor-widget-text-editor">
                                     <div class="elementor-widget-container">
-                                        <p>Una vez realizado un reclamo, pueden esperar una respuesta por correo
-                                            electrónico o
-                                            llamada telefónica. Además, estamos disponibles a través de nuestras oficinas,
-                                            correo y teléfono convencional para cualquier comunicación adicional.
-                                            Agradecemos
-                                            sus aportaciones, ya que nos ayudan a mejorar nuestros servicios constantemente.
+                                        <p>{{$data['subtitulo_seccion']}}
                                             </p>
                                     </div>
                                 </div>
